930f407a7ebebf879885c47cfd7619e48fecd52c
Gilberto Souza <gilbertoghmknfs@gmail.com>
2022-05-15 19:52:21 -0300

    Adicionando testes swaggers, e adicionando rotas no controller do professor

--- Exercicio2/src/main/java/com/exercicio2/Exercicio2/Controllers/ControllerProfessor.java
+++ Exercicio2/src/main/java/com/exercicio2/Exercicio2/Controllers/ControllerProfessor.java
@@ -1,21 +1,33 @@
 package com.exercicio2.Exercicio2.Controllers;
 
 import com.exercicio2.Exercicio2.Models.ModelProfessor;
+import com.exercicio2.Exercicio2.Repository.RepositoryProfessor;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+import java.util.*;
 
 @RestController
 @RequestMapping("/professor")
+@Api(value = "Professor")
 public class ControllerProfessor {
- 
+
+    @Autowired
+    private RepositoryProfessor professorRepository;
+
     @GetMapping
-    public String getControllerProfessor() {
-        return "Professor";
+    @ApiOperation(value = "Retorna uma lista de Professores")
+    public List<ModelProfessor> getProfessores(){
+        return professorRepository.findAll();
     }
 
     @PostMapping
-    public ModelProfessor CreateProfessor(@RequestBody ModelProfessor professor) {
-        ModelProfessor newProfessor = new ModelProfessor();
-        return newProfessor;
-    }  
+    @ApiOperation(value = "Adicionar um Professor")
+    public ModelProfessor createProfessor(@RequestBody ModelProfessor prof) {
+        return professorRepository.save(prof);
+    }
 }
